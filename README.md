83729fb9e31f0a45d10865998992bb2dfd2df293
antonhansel <antoninribeaud@gmail.com>
2016-05-06 20:02:06 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Amnesia-node
+======
+
+Opinionated caching module for NodeJs using Redis.
+
+
+## Installation
+```sh
+npm install amnesia-node --save
+```
+
+You're done. Now stop being lazy and go write some code.
---
+++ test/dummy.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var should = require('should');
+var amnesia = require('../lib/');
+
+describe('some test', function() {
+  it("should work", function(done) {
+    done(new Error("Noice"));
+  });
+});
